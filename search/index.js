adf985e1a6d67295e85fbb4e69cf0e271c849927
Tom MacWright <tom@macwright.org>
2018-12-06 22:13:58 -0800

    v2

---
+++ search/index.js
@@ -0,0 +1,36 @@
+const rateLimit = require("micro-ratelimit");
+const { parse } = require("url");
+const { parse: parseQuery } = require("querystring");
+const { guess, collapseResults, methods } = require("./api.js");
+const Context = require("./context.js");
+const makeSources = require("./sources/index.js");
+
+async function handler(req) {
+  const { pathname, query } = parse(req.url);
+  const { id, type } = parseQuery(query);
+  const ctx = new Context();
+  const sources = makeSources(ctx);
+  const method = methods.find(method => method.id === type);
+  if (method) {
+    ctx.time(`overall id=${id} type=${type}`);
+    const results = await method.resolve(ctx, id, sources);
+    ctx.timeEnd(`overall id=${id} type=${type}`);
+    const { ids, permalinks } = collapseResults(results);
+    return {
+      messages: ctx.getMessages(),
+      results: ids,
+      permalinks
+    };
+  }
+  if (type === "guess") {
+    return await guess(ctx, id);
+  }
+}
+
+module.exports = rateLimit(
+  { window: 10000, limit: 100, headers: true },
+  handler
+);
+
+module.exports.handler = handler;
+module.exports.Context = Context;
